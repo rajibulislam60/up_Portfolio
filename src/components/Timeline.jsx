9a1264e5f355b9b10de65ac5cf5b4b08cf04e04f
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-10-31 17:59:15 +0600

    add some certificated

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Simplilearn from "./../../public/simplilearn.jpg";
 import Creative from "./../../public/creative.jpg";
+import Bohubrihi from "/Bcertificated.png";
+import G_learning from "/great learning certificated.png";
 
 const Timeline = () => {
-     const [currentCertificate, setCurrentCertificate] = useState(null);
+  const [currentCertificate, setCurrentCertificate] = useState(null);
 
-    const handleCertificateShow = (certificate) => {
-      setCurrentCertificate(certificate);
-    };
+  const handleCertificateShow = (certificate) => {
+    setCurrentCertificate(certificate);
+  };
   return (
     <div className="w-full border-[3px] mx-auto relative mt-[90px]">
       <div className="uppercase text-[32px] font-semibold leading-[36px] bg-white absolute top-[-30px] left-[50px]">
@@ -88,9 +90,79 @@ const Timeline = () => {
             )}
           </div>
         </div>
+        <div className="flex justify-between items-center mt-[30px]">
+          <div>
+            <h3 className="text-[24px] font-semibold leading-[28px] text-black">
+              Bohubrihi
+            </h3>
+            <h5 className="text-[20px] font-medium leading-[24px] mt-[14px]">
+              Introduction to JavaScript
+            </h5>
+          </div>
+          <div>
+            <button
+              onClick={() => handleCertificateShow("Bohubrihi")}
+              className="w-[140px] text-[20px] font-medium leading-[24px] border text-center px-2 py-2"
+            >
+              2024
+            </button>
+            {currentCertificate === "Bohubrihi" && (
+              <div className="fixed inset-0 bg-black/60 flex justify-center items-center z-50">
+                <div className="w-[800px] bg-white text-center p-[50px] relative">
+                  <button
+                    onClick={() => setCurrentCertificate(null)}
+                    className="bg-red-500 px-2 rounded-full absolute top-4 right-4 text-white"
+                  >
+                    X
+                  </button>
+                  <img
+                    src={Bohubrihi}
+                    alt="certificate"
+                    className="w-full h-auto mb-4"
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="flex justify-between items-center mt-[30px]">
+          <div>
+            <h3 className="text-[24px] font-semibold leading-[28px] text-black">
+              Great Learning
+            </h3>
+            <h5 className="text-[20px] font-medium leading-[24px] mt-[14px]">
+              Front End Developer -HTML
+            </h5>
+          </div>
+          <div>
+            <button
+              onClick={() => handleCertificateShow("G_learning")}
+              className="w-[140px] text-[20px] font-medium leading-[24px] border text-center px-2 py-2"
+            >
+              2024
+            </button>
+            {currentCertificate === "G_learning" && (
+              <div className="fixed inset-0 bg-black/60 flex justify-center items-center z-50">
+                <div className="w-[800px] bg-white text-center p-[50px] relative">
+                  <button
+                    onClick={() => setCurrentCertificate(null)}
+                    className="bg-red-500 px-2 rounded-full absolute top-4 right-4 text-white"
+                  >
+                    X
+                  </button>
+                  <img
+                    src={G_learning}
+                    alt="certificate"
+                    className="w-full h-auto mb-4"
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Timeline
+export default Timeline;
