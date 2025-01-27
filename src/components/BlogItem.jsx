4a197f5b5e36119333c5464184eeca0caedcbe38
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-01-27 16:52:27 +0600

    doing update like add json fermat

--- src/components/BlogItem.jsx
+++ src/components/BlogItem.jsx
@@ -2,45 +2,63 @@ import React from 'react'
 
 
 const BlogItem = ({ href, src, title, date, videoSrc }) => {
+  let workData = [{
+    id:1,
+    image:"/web1.png",
+    name:"TeaPoz",
+    details:"How to create Web Development project using React",
+    date:"Sept 10, 2024",
+    link:"https://teapoz.vercel.app/",
+    creater:"Rajibul Islam"
+  },
+  {
+    id:2,
+    image:"/web2.png",
+    name:"TeaPoz",
+    details:"How to create Web Development project using React",
+    date:"Sept 10, 2024",
+    link:"https://teapoz.vercel.app/",
+    creater:"Rajibul Islam"
+  }
+
+]
   return (
-    <div className="flex items-center gap-10 mt-[30px]">
+    <>
+      <div>
+        {workData.map((item)=>(
+          <div key={item.id}>
+            <div className="flex items-center gap-10 mt-[30px]">
       <div className="w-[48%] h-[250px] relative">
         <div className=" w-full h-full overflow-hidden">
-          <a href={href} target="_blank" rel="noopener noreferrer">
-            <img
-              className="w-full h-full cursor-pointer duration-500 ease-in-out hover:scale-110 relative"
-              src={src}
-              alt="Portfolio Image 1"
-            />
-            <video
-              className="w-full h-full cursor-pointer duration-500 ease-in-out hover:scale-110 relative"
-              src={videoSrc}
-             
-            />
+          <a href={item.link} target="_blank" rel="noopener noreferrer">
+            <img src={item.image} alt={item.name} />
           </a>
         </div>
         <h4 className="px-6 py-2 bg-white text-black absolute top-7 left-7">
-          {date}
+          {item.date}
         </h4>
       </div>
       <div className="w-[48%]">
         <h5 className="text-[20px] font-medium leading-[24px] text-black/50">
           by{" "}
           <span className="text-[24px] font-semibold leading-[28px]">
-            Rajibul Islam Rajib
+            {item.creater}
           </span>
         </h5>
         <div className="w-full h-[2px] bg-black/20 mt-5 mb-5"></div>
-
-        <h2 className="text-[24px] font-semibold leading-[28px]">{title}</h2>
-        <a href={href} target="_blank" rel="noopener noreferrer">
+        <h2 className="text-[24px] font-semibold leading-[28px]">{item.name}</h2>
+        <h2 className="text-[18px] font-medium leading-[24px] mt-2">{item.details}</h2>
+        <a href={item.link} target="_blank" rel="noopener noreferrer">
           <button className="text-[24px] font-medium leading-[28px] border px-[30px] py-2 hover:bg-black/50 hover:text-white duration-[0.8s] mt-[30px]">
           Read more
         </button>
         </a>
-        
       </div>
     </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
 import BlogItem from '../components/BlogItem';
-import Web1 from "./../../public/web1.png";
-import Web2 from "./../../public/web2.png";
-import Web3 from "./../../public/web3.png";
-import Web4 from "./../../public/web4.png";
-import Web5 from "./../../public/product_cart.png";
-import video1 from "./../../public/project_video.mp4";
 
 const Work = () => {
   return (
@@ -16,44 +10,7 @@ const Work = () => {
             <div className="uppercase text-[32px] font-semibold leading-[36px] bg-white absolute top-[-30px] left-[50px]">
               <h3 className="bg-black/15 px-[30px] py-2">working project</h3>
             </div>
-            <div className="px-[50px] py-[80px]">
-              <BlogItem
-                href="https://teapoz.vercel.app/"
-                src={Web1}
-                title="How to create Web Development project using React"
-                date="Sept 10, 2024"
-              />
-              <BlogItem
-                href="https://nexcent-rouge.vercel.app/"
-                src={Web2}
-                title="How to create Web Development project using React"
-                date="Sept 10, 2024"
-              />
-              <BlogItem
-                href="https://dark-mode-azure.vercel.app/"
-                src={Web3}
-                title="How to create Web Development project using React"
-                date="Sept 10, 2024"
-              />
-              <BlogItem
-                href="https://corewave-react.vercel.app/"
-                src={Web4}
-                title="How to create Web Development project using React"
-                date="Sept 10, 2024"
-              />
-              <BlogItem
-                href="https://front-end-test-task-beryl.vercel.app/"
-                src={Web5}
-                title="This project work by Vanilla JavaScript."
-                date="Dec 20, 2024"
-              />
-              <BlogItem
-                href="https://news-paper-one.vercel.app/"
-                videoSrc={video1}
-                title="This is a newspapar web site, it's work by React.js frame work and functionallty work by JavaScript."
-                date="Dec 25, 2024"
-              />
-            </div>
+            <BlogItem/>
           </div>
         </div>
       </div>
